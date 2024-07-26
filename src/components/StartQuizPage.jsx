f5a6c34fc728930f0d3e05644cf9615285e4c4be
Ian <ian_frye@outlook.com>
2024-07-26 11:09:50 -0400

    removing placeholder on start quiz page

--- src/components/StartQuizPage.jsx
+++ src/components/StartQuizPage.jsx
@@ -4,7 +4,6 @@ export default function StartQuizPage({ setInQuiz }) {
   return (
     <div className='card'>
       <h1>Quizzical</h1>
-      <p>Some description if needed</p>
       <button onClick={() => setInQuiz(true)}>Start quiz</button>
     </div>
   );
